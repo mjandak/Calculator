7ab7f5959d6ac6b70fcc10885b0f80381cbb7237
mirek.jandak <mirek.jandak@gmail.com>
2022-10-22 16:15:22 +0200

    entry issues

---
+++ CalculatorMAUI/HideSoftInputBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorMAUI
+{
+    public class HideSoftInputBehaviour : Behavior<Entry>
+    {
+        protected override void OnAttachedTo(Entry entry)
+        {
+            entry.Focused += Entry_Focused;
+            base.OnAttachedTo(entry);
+
+        }
+
+        private void Entry_Focused(object sender, FocusEventArgs e)
+        {
+            
+            DependencyService.Get<IKeyboardHelper>().HideKeyboard();
+        }
+    }
+}
