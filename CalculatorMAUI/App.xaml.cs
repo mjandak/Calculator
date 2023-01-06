00f046e0298d942812d0b94f3c5433b5c6711658
mirek.jandak <mirek.jandak@gmail.com>
2023-01-06 08:21:31 +0100

    code clean up

--- CalculatorMAUI/App.xaml.cs
+++ CalculatorMAUI/App.xaml.cs
@@ -10,46 +10,15 @@ namespace CalculatorMAUI
         public static void SetTheme(Themes theme)
         {
             var mergedDictionaries = Current.Resources.MergedDictionaries;
-            mergedDictionaries.Clear();
-
-            //Microsoft.Maui.Controls.Xaml.Extensions.LoadFromXaml()
-
-            //ResourceDictionary rd = (ResourceDictionary)XamlReader.Load(System.Xml.XmlReader.Create("skin.xaml")); 
-            //mergedDictionaries.Add(rd);
-
-            //ResourceDictionary item = new ResourceDictionary()
-            //{
-            //    Source = new Uri("/CalculatorMAUI; component/Styles/Colors.xaml", UriKind.RelativeOrAbsolute)
-            //};
-            //mergedDictionaries.Add(item);
-            //mergedDictionaries.Add(new ResourceDictionary()
-            //{
-            //    Source = new Uri("/CalculatorMAUI; component/Styles/Styles.xaml", UriKind.RelativeOrAbsolute)
-            //});
-
-            //if (currentTheme != null)
-            //{
-            //    mergedDictionaries.Remove(currentTheme);
-            //}
             if (theme == Themes.Simple)
             {
-                //currentTheme = new Simple();
-                //mergedDictionaries.Add(currentTheme);
-                mergedDictionaries.Add(new Simple());
-                //mergedDictionaries.Add(new ResourceDictionary()
-                //{
-                //    Source = new Uri("/CalculatorMAUI; component/Themes/Simple.xaml", UriKind.RelativeOrAbsolute)
-                //});
+                currentTheme = new Simple();
+                mergedDictionaries.Add(currentTheme);
             }
             else if (theme == Themes.Retro)
             {
-                //currentTheme = new Retro();
-                //mergedDictionaries.Add(currentTheme);
-                mergedDictionaries.Add(new Retro());
-                //mergedDictionaries.Add(new ResourceDictionary()
-                //{
-                //    Source = new Uri("/CalculatorMAUI; component/Themes/Retro.xaml", UriKind.RelativeOrAbsolute)
-                //});
+                currentTheme = new Retro();
+                mergedDictionaries.Add(currentTheme);
             }
             else
             {
